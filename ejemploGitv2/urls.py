360233c7fff255e70c7fdea484ef079a02ca23f2
natalia <natalia.isabel.navarrete@gmail.com>
2023-09-19 16:06:28 -0300

    app2 terminada

--- ejemploGitv2/urls.py
+++ ejemploGitv2/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app2 import views as v2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vista2/', v2.vistaDos),
 ]
